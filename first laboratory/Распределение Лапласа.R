3fbeca103359e8d53c72c2f30de6c7cb09ae2ece
GareevaMilena <61157126+GareevaMilena@users.noreply.github.com>
2020-03-01 20:39:45 +0300

    Add files via upload

--- first laboratory/Распределение Лапласа.R
+++ first laboratory/Распределение Лапласа.R
@@ -1,8 +1,8 @@
 n = 10;
-D <- DExp(rate = 1/sqrt(2))
-LaplaceNumbers <-r(D)(n)
-a = min(LaplaceNumbers)
-b = max(LaplaceNumbers)
+li;
+LaplaceNumbers <-r(D)(n);
+a = min(LaplaceNumbers);
+b = max(LaplaceNumbers);
 m = round(1 + 3.322 * log10(n), 0);
 step = (b - a) / m;
 count = (b - a) / step;
@@ -10,6 +10,6 @@ x = array(c(T,F),dim=c(count));
 for(i in 0 : count){
   x[i] = a + i * step
 }
-hist(LaplaceNumbers, probability = TRUE, col = "blue")
-curve(d(D)(x), col = "red", add = TRUE)
-grid()
+hist(LaplaceNumbers, probability = TRUE, col = "blue");
+curve(d(D)(x), col = "red", add = TRUE);
+grid();
